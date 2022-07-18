de2efbf16e942781a42ced53bb1a265d342ced1a
ISJSHSI9991 <109557398+ISJSHSI9991@users.noreply.github.com>
2022-07-18 15:50:07 -0700

    Update utils.py

--- lib/utils.py
+++ lib/utils.py
@@ -75,7 +75,7 @@ def make_embed(group_info, date):
             dict(name="Group Members", value=group_info["memberCount"])
         ],
         footer=dict(
-            text="By Chrisnel Private"
+            text="Boblox Hub Finder©"
         ),
         timestamp=date.isoformat()
     )
